7e087cb7908517fc873c762625cd0a072c69578d
albertflavian <popa.albertflavian@gmail.com>
2019-05-21 16:11:21 +0300

    css+js+images

---
+++ MyCinema/Scripts/admin-program-ip.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    var deleteButton = document.querySelector("#button-delete");
+
+    var checkboxes = document.querySelectorAll(".check-for-delete");
+
+    function totalChecks() {
+        var total = 0;
+
+        for (var i = 0; i < checkboxes.length; ++i)
+            if (checkboxes[i].checked)
+                total++;
+
+        return total;
+    }
+
+    function renameButton()
+    {
+        var checks = totalChecks();
+
+        if (checks > 1) {
+            deleteButton.innerHTML = "Delete " + checks + " Programs";
+            deleteButton.disabled = false;
+        }
+        else if (checks == 1) {
+            deleteButton.innerHTML = "Delete Program";
+            deleteButton.disabled = false;
+        }
+        else {
+            deleteButton.innerHTML = "Delete Program";
+            deleteButton.disabled = true;
+        }
+    }
+
+    deleteButton.disabled = true;
+
+    checkboxes.forEach(function(elem) {
+        elem.checked = false;
+        elem.addEventListener("click", renameButton);
+    });
+});
+
---
+++ MyCinema/Scripts/admin-rooms-ip.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    var deleteButton = document.querySelector("#button-delete");
+
+    var checkboxes = document.querySelectorAll(".check-for-delete");
+
+    function totalChecks() {
+        var total = 0;
+
+        for (var i = 0; i < checkboxes.length; ++i)
+            if (checkboxes[i].checked)
+                total++;
+
+        return total;
+    }
+
+    function renameButton()
+    {
+        var checks = totalChecks();
+
+        if (checks > 1) {
+            deleteButton.innerHTML = "Delete " + checks + " Rooms";
+            deleteButton.disabled = false;
+        }
+        else if (checks == 1) {
+            deleteButton.innerHTML = "Delete Room";
+            deleteButton.disabled = false;
+        }
+        else {
+            deleteButton.innerHTML = "Delete Room";
+            deleteButton.disabled = true;
+        }
+    }
+
+    deleteButton.disabled = true;
+
+    checkboxes.forEach(function(elem) {
+        elem.checked = false;
+        elem.addEventListener("click", renameButton);
+    });
+});
+
