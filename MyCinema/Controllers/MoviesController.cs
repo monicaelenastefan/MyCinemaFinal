8308beb818896e71680849b70f70bbb8d06fcde8
Monica Stefan <monicaelenastefan@gmail.com>
2019-05-22 14:18:58 +0300

    movie user view

--- MyCinema/Controllers/MoviesController.cs
+++ MyCinema/Controllers/MoviesController.cs
@@ -16,8 +16,33 @@ namespace MyCinema.Controllers
     {
         private MyModel db = new MyModel();
 
-        // GET: Movies
+
         public ActionResult Index()
+        {
+            ViewBag.EmailID = Session["name"];
+            String nume;
+            nume = ViewBag.EmailID;
+            var db = new MyModel();
+            var usr = (from u in db.Users
+                       where u.EmailID == nume
+                       select u).FirstOrDefault();
+
+            if (usr != null)
+            {
+                ViewBag.FirstName = usr.FirstName;
+                ViewBag.LastName = usr.LastName;
+                ViewBag.UserName = usr.Username;
+
+            }
+
+            var item = (from d in db.Movies
+                        select d).ToList();
+            ViewData["Movies"] = db.Movies.ToList();
+            return View(item);
+        }
+
+        // GET: Movies
+        public ActionResult IndexAdmin()
         {
             
 
